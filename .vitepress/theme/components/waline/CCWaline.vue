263af067c6d7d55280135957ba812b67b2b5ec94
Seeridia <Seeridia@gmail.com>
2026-01-28 17:00:11 +0800

    feat: 更新 Waline 组件样式，增强视觉效果和布局

--- .vitepress/theme/components/waline/CCWaline.vue
+++ .vitepress/theme/components/waline/CCWaline.vue
@@ -13,6 +13,19 @@ const { serverURL, path } = useWalineBase();
 
 <style scoped>
 [data-waline] {
+  --waline-theme-color: var(--vp-c-brand-1);
+  --waline-active-color: var(--vp-c-brand-2);
+  --waline-white: var(--vp-button-brand-text);
+  --waline-color: var(--vp-c-text-1);
+  --waline-dark-grey: var(--vp-c-text-1);
+  --waline-info-bg-color: var(--vp-c-bg-soft);
+  --waline-info-color: var(--vp-c-text-2);
+  --waline-light-grey: var(--vp-c-text-2);
+  --waline-border-color: var(--vp-c-divider);
+  --waline-bg-color: transparent;
+}
+
+:deep(button) {
   --waline-theme-color: var(--vp-button-brand-bg);
   --waline-active-color: var(--vp-button-brand-hover-bg);
 }
@@ -23,8 +36,59 @@ const { serverURL, path } = useWalineBase();
   }
 }
 
-:deep(.wl-panel) {
-  margin: 8px 0px;
-  border-radius: 8px;
+:deep(.wl-comment) {
+  align-items: flex-end;
+
+  .wl-login-info {
+    margin: 0.75em 0.75em;
+  }
+
+  .wl-panel {
+    margin: 8px 0px;
+    border-radius: 8px;
+
+    .wl-header {
+      border-bottom: 1px solid var(--waline-border-color);
+
+      label,
+      input {
+        font-size: 12px;
+      }
+    }
+
+    .wl-editor {
+      padding: 0.75em;
+    }
+  }
+}
+
+:deep(.wl-cards) {
+  .wl-card {
+    border-bottom: 1px solid var(--waline-border-color);
+
+    .wl-head {
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+
+      .wl-badge {
+        border-color: var(--vp-badge-tip-border);
+        color: var(--vp-badge-tip-text);
+        background-color: var(--vp-badge-tip-bg);
+        white-space: nowrap;
+        border-radius: 12px;
+        padding: 0 8px;
+        font-size: 10px;
+        font-weight: 500;
+        display: inline-block;
+      }
+
+      .wl-comment-actions {
+        margin-left: auto;
+        display: flex;
+        gap: 0.5rem;
+      }
+    }
+  }
 }
 </style>
